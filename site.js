fe1b28d6e38d10cec7306bdc790c340ea980fc82
Charlotte <charlottestantonrpi@gmail.com>
2019-11-08 21:36:17 -0500

    far from done but wanted to push

--- site.js
+++ site.js
@@ -1,3 +1,8 @@
 /* Furnitrade Site JavaScript File 
    Everything here should be in functions that are well
    commented so this file can be used throughout the site */
+
+   function loggedin(){
+      //the button of the class account will change what it says if the user has
+      //created an account????
+   }
